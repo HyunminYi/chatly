43606d00f722d0159ea287f1e859690f95b80d5d
HyunminYi <smurf2@kakao.com>
2025-04-06 22:55:26 +0900

    add middleware

---
+++ src/app/constants/routes.ts
@@ -0,0 +1,9 @@
+export const BASE_URL = "/";
+export const AUTH_URL = {
+  LOGIN: "/login",
+  SIGN_UP: "/signup",
+};
+export const CHAT_ROUTES = {
+  CONVERSATIONS: "/c",
+};
+export const PUBLIC_ROUTES = [AUTH_URL.LOGIN, AUTH_URL.SIGN_UP];
