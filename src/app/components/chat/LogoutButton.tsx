c0661d26a55f9a42bb966281c6f373e10c7030ce
HyunminYi <smurf2@kakao.com>
2025-04-08 05:13:30 +0900

    wip:markup logout button

---
+++ src/app/components/chat/LogoutButton.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/app/components/ui/button";
+
+const LogoutButton = () => {
+  return (
+    <>
+      <Button className="w-full">로그아웃</Button>
+    </>
+  );
+};
+
+export default LogoutButton;
