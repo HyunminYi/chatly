b70468d153fadaed0f26857919085934f15946e8
HyunminYi <smurf2@kakao.com>
2025-04-21 01:13:23 +0900

    Message 메모이제이션 추가

--- src/app/components/chat/Message.tsx
+++ src/app/components/chat/Message.tsx
@@ -3,35 +3,51 @@ import {
   AvatarImage,
   Avatar,
 } from "@/app/components/ui/avatar";
+import { memo, useMemo } from "react";
 
-type Props = {
+// type Props = {
+//   name?: string;
+//   content?: string;
+//   role: string;
+// };
+
+interface IProps {
   name?: string;
   content?: string;
   role: string;
-};
-const Message = ({ name = "User", content = "", role = "user" }: Props) => {
-  const isAssistant = role === "assistant";
-  const avatarName = isAssistant ? "CHATLY" : name;
-  return (
-    <div className="flex items-start gap-2 mb-5">
-      {/*  아바타  */}
-      <Avatar>
-        <AvatarImage
-          src={isAssistant ? "/logo2.png" : ""}
-          alt="avatar"
-          className="scale-90"
-        />
-        <AvatarFallback>{avatarName[0]}</AvatarFallback>
-      </Avatar>
-      {/*  이름 + 내용 */}
-      <div>
-        <h2 className="font-normal text-base">{avatarName}</h2>
-        <div className="mt-2 whitespace-break-spaces font-light text-sm  leading-loose tracking-tight">
-          {content}
+}
+const Message = memo(
+  ({ name = "User", content = "", role = "user" }: IProps) => {
+    // const isAssistant = role === "assistant";
+    // const avatarName = isAssistant ? "CHATLY" : name;
+
+    const { avatarName, avatarSrc } = useMemo(() => {
+      const isAssistant = role === "assistant";
+      const avatarName = isAssistant ? "CHATLY" : name;
+      const avatarSrc = isAssistant ? "/logo2.png" : "";
+      return {
+        avatarName,
+        avatarSrc,
+      };
+    }, [role, name]);
+
+    return (
+      <div className="flex items-start gap-2 mb-5">
+        {/*  아바타  */}
+        <Avatar>
+          <AvatarImage src={avatarSrc} alt="avatar" className="scale-90" />
+          <AvatarFallback>{avatarName[0]}</AvatarFallback>
+        </Avatar>
+        {/*  이름 + 내용 */}
+        <div>
+          <h2 className="font-normal text-base">{avatarName}</h2>
+          <div className="mt-2 whitespace-break-spaces font-light text-sm  leading-loose tracking-tight">
+            {content}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  },
+);
 
 export default Message;
