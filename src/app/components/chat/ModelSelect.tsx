01f4b0484e293a7d34d3fdaa0fa32d3f4d2f293d
HyunminYi <smurf2@kakao.com>
2025-04-23 03:15:32 +0900

    주석제거,토글 커스텀훅, value 기본값 추가

--- src/app/components/chat/ModelSelect.tsx
+++ src/app/components/chat/ModelSelect.tsx
@@ -1,4 +1,5 @@
 "use client";
+//-# toggle / model store -> custom hook 변경, placeholder (빈값) 에서 기본 선택 지정
 import {
   Select,
   SelectContent,
@@ -8,43 +9,18 @@ import {
   SelectValue,
 } from "@/app/components/ui/select";
 import { useModelStore } from "@/app/store/model";
-import { useEffect } from "react";
-import { ValueOf } from "next/constants";
 import { modelMap, MODELS, tModelId, tModelName } from "@/app/types/model";
-import { useToggleStore } from "@/app/store/toggle";
 import SelectAlert from "@/app/components/chat/SelectAlert";
-
-// export const MODELS = [
-//   "Claude3.5 Haiku",
-//   "Claude3.5 Sonnet",
-//   "Claude3 Sonnet",
-//   "Claude3 Haiku",
-// ] as const;
-//
-// export type tModelName = (typeof MODELS)[number];
-//
-// const modelMap: Record<tModelName, string> = {
-//   "Claude3.5 Haiku": "claude-3-5-haiku-20241022",
-//   "Claude3.5 Sonnet": "claude-3-5-sonnet-20241022",
-//   "Claude3 Haiku": "claude-3-haiku-20240307",
-//   "Claude3 Sonnet": "claude-3-sonnet-20240229",
-// };
+import useModal from "@/app/hooks/modal/useModalStore";
 
 const ModelSelect = () => {
-  // const { model: currentModel, updateModel } = useModelStore((state) => ({
-  //   model: state.model,
-  //   updateModel: state.updateModel,
-  // }));
   const currentModel = useModelStore((state) => state.model);
   const updateModel = useModelStore((state) => state.updateModel);
-  const updateToggle = useToggleStore((state) => state.updateToggle);
-  const toggle = useToggleStore((state) => state.toggle);
+  const { onOpen, open } = useModal();
 
   const handleChange = (selectModel: tModelName) => {
-    // updateToggle(true);
     const model = selectModel;
     const modelId = modelMap[model];
-    // console.log(model, modelId);
     updateModel({
       model,
       modelId,
@@ -54,9 +30,9 @@ const ModelSelect = () => {
   return (
     <div className="flex items-center gap-2">
       <Select
-        value={currentModel ?? ""}
+        value={currentModel ?? "Claude3.5 Haiku"}
         onValueChange={handleChange}
-        onOpenChange={() => updateToggle(true)}
+        onOpenChange={onOpen}
       >
         <SelectTrigger className="w-[180px] border-none text-sm focus:ring-transparent">
           <SelectValue placeholder="모델 선택" />
@@ -76,7 +52,7 @@ const ModelSelect = () => {
           </SelectGroup>
         </SelectContent>
       </Select>
-      {!toggle ? <SelectAlert /> : null}
+      {!open ? <SelectAlert /> : null}
     </div>
   );
 };
