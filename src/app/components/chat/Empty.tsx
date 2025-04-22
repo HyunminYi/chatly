5a02e9c0f25d5dba3a1aac0b4f6eabe5b389493c
HyunminYi <smurf2@kakao.com>
2025-04-23 03:12:22 +0900

    logo 변경

--- src/app/components/chat/Empty.tsx
+++ src/app/components/chat/Empty.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Empty = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
-      <Image src="/logo2.png" width={50} height={50} alt="empty" />
+      <Image src="/logo.png" width={50} height={50} alt="empty" />
       <h3 className="text-xl md:text-2xl font-bold">무엇을 도와드릴까요 ?</h3>
     </div>
   );
