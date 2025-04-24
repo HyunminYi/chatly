b2e25a3a6f5c8ab6aa5414bb6e2cde1f4c092ee5
HyunminYi <smurf2@kakao.com>
2025-04-24 09:05:23 +0900

    타입 설정

--- src/app/actions/sessions.ts
+++ src/app/actions/sessions.ts
@@ -2,20 +2,22 @@
 //-# next 14 -> 15 cookies()비동기 처리
 // jwt 생성 검증 쿠키 세팅 삭제 로직
 
-import { jwtVerify, SignJWT } from "jose";
+import { JWTPayload, jwtVerify, SignJWT } from "jose";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { AUTH_URL } from "@/app/constants/routes";
+import { TUser } from "@/app/types/db";
 
-type SessionPayload = {
+export interface ISessionPayload extends TUser {
   id: string;
   name: string;
-};
+  [key: string]: any;
+}
 const secretKey = process.env.SESSION_SECRET;
 const key = new TextEncoder().encode(secretKey); // jose 를 위한 byte 변형
 
 // 암호화
-const encrypt = async (payload: SessionPayload) => {
+const encrypt = async (payload: ISessionPayload & JWTPayload) => {
   return new SignJWT(payload)
     .setProtectedHeader({ alg: "HS256" })
     .setIssuedAt() // 발급시간을 현재로
@@ -25,8 +27,9 @@ const encrypt = async (payload: SessionPayload) => {
 
 // 검증
 export const verify = async (session: string | undefined = "") => {
+  if (!session) return null;
   try {
-    const { payload } = await jwtVerify<SessionPayload>(session, key, {
+    const { payload } = await jwtVerify<ISessionPayload>(session, key, {
       algorithms: ["HS256"],
     });
     return payload;
@@ -35,7 +38,7 @@ export const verify = async (session: string | undefined = "") => {
   }
 };
 
-export const createSession = async (payload: SessionPayload) => {
+export const createSession = async (payload: ISessionPayload) => {
   const expiresAt = new Date(Date.now() + 24 * 60 * 60 * 1000); // 24시간 후
   const session = await encrypt(payload);
   const c = await cookies();
