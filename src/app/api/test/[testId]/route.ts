059b937c81d922791c87e8f9f6ef60095f80bc77
HyunminYi <smurf2@kakao.com>
2025-04-24 09:54:17 +0900

    api test folder removed

--- src/app/api/test/[testId]/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function POST(request:Request, {params} :{params:{testId:string}}) {
-  const userData = await request.json();
-  console.log('server user Data',userData);
-  console.log('server params',params.testId);
-  return NextResponse.json({message:'response success'})
-}
